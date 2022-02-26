fdbc38ee64e24dc0d102b31f8790e039808630f2
Zoobie <kremadnia@gmail.com>
2022-02-26 12:07:06 +0100

    DELETE axios-firebase.js

--- src/config/axios-firebase.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create(
-  {
-    baseURL: 'https://quizz-creator-default-rtdb.europe-west1.firebasedatabase.app/'
-  }
-)
-
-export default instance;
