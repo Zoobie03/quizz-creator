bbe9082bb9ac3f8bad711c91e701ae55b072be99
Zoobie <kremadnia@gmail.com>
2022-03-22 17:50:54 +0100

    ADD: content in Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { LoginContext } from '../../hoc/Contexts/LoginContext';
 
 const Home = () => {
-  
+  const { user } = useContext(LoginContext);
 
   return (
-    <div>
-      <h1>Accueil</h1>
-    </div>
+    <>
+      {user ? (
+        <div>
+          <h1>Bienvenue sur Quizz-creator</h1>
+
+          <h2>
+            <u>Pour créer votre premier quizz</u>
+          </h2>
+
+          <p>Allez dans l'onglet "Dashboard", et le cliquez sur le +</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Bienvenue sur Quizz-creator</h1>
+
+          <h2>
+            <u>Commencez par créer votre compte</u>
+          </h2>
+
+          <p>Allez dans l'onglet se connecter, et cliquez sur créer un compte</p>
+        </div>
+      )}
+    </>
   );
 };
 
