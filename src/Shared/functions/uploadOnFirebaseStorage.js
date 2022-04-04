bc15952f762b51ad85cbdef3b486972d11b4b7c1
kévin <kremadnia@gmail.com>
2022-04-04 05:33:06 +0200

    update custom functions for upload on firebase storage --> now, we update profil in the same func

--- src/Shared/functions/uploadOnFirebaseStorage.js
+++ src/Shared/functions/uploadOnFirebaseStorage.js
@@ -1,4 +1,5 @@
-import { ref, uploadBytes } from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../../config/firebase';
 
 export async function uploadOnFirebaseStorage(file, currentUser, setLoading) {
@@ -7,6 +8,10 @@ export async function uploadOnFirebaseStorage(file, currentUser, setLoading) {
   setLoading(true);
 
   const snapshot = await uploadBytes(fileRef, file);
+
+  const photoURL = await getDownloadURL(fileRef);
+
+  updateProfile(currentUser, { photoURL });
 
   setLoading(false);
 }
