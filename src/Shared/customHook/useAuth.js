4d07c188111293139fbaf26ffc800fb07af9c3e6
kévin <kremadnia@gmail.com>
2022-04-04 05:24:53 +0200

    ADD commentary code

--- src/Shared/customHook/useAuth.js
+++ src/Shared/customHook/useAuth.js
@@ -2,6 +2,8 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react/cjs/react.production.min';
 import { auth } from '../../config/firebase';
 
+// Exemple use of useAuth hook
+// const currentUser = useAuth();
 export function useAuth() {
   const [currentUser, setCurrentUser] = useState();
 
