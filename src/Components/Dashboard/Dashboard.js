3ba4434c68b1bd0bf71686959e2e2f33b2bb7b14
Zoobie <kremadnia@gmail.com>
2022-04-04 15:45:15 +0200

    Fix responsiv design

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
 // Own files
 import { db } from '../../config/firebase';
 import styles from './Dashboard.module.css';
+import useAuth from '../../customHook/useAuth';
 // Hoc
 import { LoginContext } from '../../hoc/Contexts/LoginContext';
 // Components
@@ -24,6 +25,7 @@ const Dashboard = () => {
 
   // Variables
   const userDoc = doc(db, 'users', uid);
+  const currentUser = useAuth();
 
   // Methods
   const fetchUserQuizzs = async () => {
@@ -93,23 +95,39 @@ const Dashboard = () => {
     setModalIsOpen(false);
   };
 
-  return modalIsOpen ? ( // PENSER A CHANGER LA CONDITION
+  return modalIsOpen ? (
     <QuizzModal modalIsOpen={modalIsOpen} onSvgClick={onSvgClickHandler} />
   ) : (
-    <div className={styles.Dashboard}>
-      <h1>Dashboard {user.displayName}</h1>
-      <ul className={styles.wrapperQuizzs}>
-        {quizzsMapped.length > 0 ? quizzsMapped : <h2>Vous n'avez aucuns Quizz pour l'instant</h2>}
+    <div className={styles.wrapperContent}>
+      <div className={styles.LeftColumnContent}>
+        {currentUser?.photoURL ? (
+          <img
+            src={currentUser?.photoURL}
+            alt='URLpicture of userProfil'
+            className={styles.profilPicture}
+          />
+        ) : null}
+      </div>
+      <div className={styles.Dashboard}>
+        <h1>Dashboard {user.displayName}</h1>
+        <ul className={styles.wrapperQuizzs}>
+          {quizzsMapped.length > 0 ? (
+            quizzsMapped
+          ) : (
+            <h2>Vous n'avez aucuns Quizz pour l'instant</h2>
+          )}
 
-        <div onClick={addQuizzClickHandler} className={styles.svg}>
-          <svg width='150px' height='150px' viewBox='0 0 24 24'>
-            <path
-              d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z'
-              fill='#71eb4d'
-            ></path>
-          </svg>
-        </div>
-      </ul>
+          <div onClick={addQuizzClickHandler} className={styles.svg}>
+            <svg width='150px' height='150px' viewBox='0 0 24 24'>
+              <path
+                d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z'
+                fill='#71eb4d'
+              ></path>
+            </svg>
+          </div>
+        </ul>
+      </div>
+      <div className={styles.RightColumnContent}></div>
     </div>
   );
 };
