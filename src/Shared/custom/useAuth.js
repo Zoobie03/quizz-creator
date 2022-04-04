2db2ed55e9ae60582919d16e71a467cad0b6f89c
kévin <kremadnia@gmail.com>
2022-04-04 05:14:03 +0200

    Create custom hook --> get current user

---
+++ src/Shared/custom/useAuth.js
@@ -0,0 +1,15 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react/cjs/react.production.min';
+import { auth } from '../../config/firebase';
+
+export function useAuth() {
+  const [currentUser, setCurrentUser] = useState();
+
+  // ComponentDidMount
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => setCurrentUser(user));
+    return unsubscribe;
+  }, []);
+
+  return currentUser;
+}
