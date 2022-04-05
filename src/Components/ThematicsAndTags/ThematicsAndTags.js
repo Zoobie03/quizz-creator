57a43358a9033c576975c5a33a9a68df6d164c44
Zoobie <kremadnia@gmail.com>
2022-04-05 07:48:53 +0200

    New component --> handle tags and thematics

--- src/Components/ThematicsAndTags/ThematicsAndTags.js
+++ src/Components/ThematicsAndTags/ThematicsAndTags.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 
-const ThematicsAndTags = () => {
-  return <div>ThematicsAndTags</div>;
+const ThematicsAndTags = ({ title, noneMessage }) => {
+  // State
+  const [thematicOrTags, setThematicsOrTags] = useState([]);
+
+  // Methods
+  const handleClick = () => {
+    console.log('Thematic clicked');
+  };
+
+  const themOrTagsMapped = thematicOrTags.map((thematicOrTag, index) => {
+    return (
+      <li key={index} onClick={handleClick}>
+        {thematicOrTag}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ul>{themOrTagsMapped.length > 0 ? themOrTagsMapped : <p>{noneMessage}</p>}</ul>
+    </div>
+  );
 };
 
 export default ThematicsAndTags;
