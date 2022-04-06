519d4c9656bf36eb85aa878642c5deef07840046
Zoobie <kremadnia@gmail.com>
2022-04-06 08:11:59 +0200

    End ignoring firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyDsnrd890YIu3hQUtlD1639Ibrd1j7VGiE',
+  authDomain: 'quizz-creator.firebaseapp.com',
+  databaseURL: 'https://quizz-creator-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'quizz-creator',
+  storageBucket: 'quizz-creator.appspot.com',
+  messagingSenderId: '366969350230',
+  appId: '1:366969350230:web:e558454f343ad82c34f3f7',
+  measurementId: 'G-3QDLQDZCY5',
+});
+
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
