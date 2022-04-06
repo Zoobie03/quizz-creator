34e4a3aacf67bec0e24c6a41b8bdb0840395a9a1
Zoobie <kremadnia@gmail.com>
2022-04-06 07:34:46 +0200

    Create new component for all quizz card

---
+++ src/Components/Dashboard/QuizzCreator/QuizzCard/QuizzCard.js
@@ -0,0 +1,17 @@
+// LIbrary
+import React from 'react';
+// Own Files
+import styles from './QuizzCard.module.css';
+
+const QuizzCard = (props) => {
+  return (
+    <div className={styles.QuizzCard}>
+      <li key={props.id}>
+        <span onClick={props.onClick}>suprrimer Quizz n {props.index}</span>
+      </li>
+      <span className={styles.quizzTitle}>{props.quizzTitle}</span>
+    </div>
+  );
+};
+
+export default QuizzCard;
