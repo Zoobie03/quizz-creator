33fd70105e21d2e94b16b8d6d60049f0a15b57c1
kévin <kremadnia@gmail.com>
2022-04-08 01:35:56 +0200

    handle search value with hook and handler function

--- src/Components/Dashboard/LeftColumnDashboard/SearchBar/SearchBar.js
+++ src/Components/Dashboard/LeftColumnDashboard/SearchBar/SearchBar.js
@@ -1,13 +1,19 @@
 // Library
-import React from 'react';
+import React, { useState } from 'react';
 // Own files
 import styles from './SearchBar.module.css';
 import loupe from '../../../../pictures/loupe.png';
 
 const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className={styles.SearchBar}>
-      <img src={loupe} alt='une icone de loupe' />
+      <img src={loupe} alt='une icone de loupe' value={searchValue} onChange={handleSearchChange} />
       <input type='text' placeholder='Rechercher...' />
     </div>
   );
