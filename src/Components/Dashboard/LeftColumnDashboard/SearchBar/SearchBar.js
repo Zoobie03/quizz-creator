0dbde9a989af13f4a1f2be1e5bcfd100f4ca224b
kévin <kremadnia@gmail.com>
2022-04-08 00:41:46 +0200

    Create new component with his style file

---
+++ src/Components/Dashboard/LeftColumnDashboard/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+// Library
+import React from 'react';
+// Own files
+import styles from './SearchBar.module.css';
+import loupe from '../../../../pictures/loupe.png';
+
+const SearchBar = () => {
+  return (
+    <div className={styles.SearchBar}>
+      <img src={loupe} alt='une icone de loupe' />
+      <input type='text' />
+    </div>
+  );
+};
+
+export default SearchBar;
