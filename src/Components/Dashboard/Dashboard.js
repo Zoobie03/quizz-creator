1cbb4d3814806940e63c5117f5020a602f1e7ef3
kévin <kremadnia@gmail.com>
2022-04-08 04:37:53 +0200

    Move all the logic QuizzCreator --> Dashboard (State Hoisting)

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,17 +1,112 @@
 // Library
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
 // Own Files
 import styles from './Dashboard.module.css';
+import { db } from '../../config/firebase';
+import { LoginContext } from '../../hoc/Contexts/LoginContext';
 // Components
 import LeftColumnDashboard from './LeftColumnDashboard/LeftColumnDashboard';
 import QuizzCreator from './QuizzCreator/QuizzCreator';
 import RightColumnDashboard from './RightColumnDashboard/RightColumnDashboard';
+import QuizzCard from './QuizzCreator/QuizzCard/QuizzCard';
 
 const Dashboard = () => {
+  // State
+  const [userQuizzs, setUserQuizzs] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  // Context
+  const { user } = useContext(LoginContext);
+  const { uid } = { ...user };
+  // componentDidMount
+  useEffect(() => {
+    fetchUserQuizzs();
+  }, []);
+  // Variables
+  const userDoc = doc(db, 'users', uid);
+
+  // Methods
+  const fetchUserQuizzs = async () => {
+    console.log('FETCH USER QUIZZ');
+    const docSnap = await getDoc(userDoc);
+    if (docSnap.exists()) {
+      // console.log('Fetching ... Document ==> ', docSnap.data());
+
+      const newState = [...docSnap.data().quizzs];
+      setUserQuizzs(newState);
+
+      return docSnap.data();
+    } else {
+      // console.log('NO Documents ==X>');
+    }
+  };
+
+  const delHandleClick = (quizzId) => {
+    fetchUserQuizzs() // Promise
+      .then((response) => {
+        const userQuizzs = response.quizzs;
+        const userQuizzsFiltered = userQuizzs.filter((quizz) => {
+          return quizz.id !== quizzId;
+        });
+
+        setUserQuizzs(userQuizzsFiltered);
+
+        updateDoc(userDoc, {
+          quizzs: userQuizzsFiltered,
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const quizzsMapped = userQuizzs.map((quizz, index) => {
+    return (
+      <QuizzCard
+        key={index}
+        index={index}
+        quizzTitle={quizz.title !== null ? quizz.title : 'Titre du quizz'}
+        onClick={() => delHandleClick(quizz.id)}
+      />
+    );
+  });
+
+  const addQuizzClickHandler = async () => {
+    setModalIsOpen(true);
+
+    const quizzTemplate = {
+      id: Math.random(),
+      title: null,
+      questions: [],
+      tags: [],
+      thematic: [],
+      quizzPicture: null,
+    };
+
+    fetchUserQuizzs();
+
+    await setDoc(
+      userDoc,
+      {
+        quizzs: [...userQuizzs, quizzTemplate],
+      },
+      { merge: true }
+    );
+  };
+
+  const onSvgClickHandler = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className={styles.Dashboard}>
-      <LeftColumnDashboard numberOfQuizz={2} />
-      <QuizzCreator />
+      <LeftColumnDashboard numberOfQuizz={userQuizzs.length} />
+      <QuizzCreator
+        quizzsMapped={quizzsMapped}
+        addQuizzClickHandler={addQuizzClickHandler}
+        modalIsOpen={modalIsOpen}
+        setModalIsOpen={() => setModalIsOpen}
+        onSvgClickHandler={onSvgClickHandler}
+        user={user}
+      />
       <RightColumnDashboard />
     </div>
   );
--- src/Components/Dashboard/QuizzCreator/QuizzCreator.js
+++ src/Components/Dashboard/QuizzCreator/QuizzCreator.js
@@ -1,111 +1,24 @@
 // Librairies
-import React, { useContext, useEffect, useState } from 'react';
-import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
+import React from 'react';
 // Own files
-import { db } from '../../../config/firebase';
 import styles from './QuizzCreator.module.css';
-// Hoc
-import { LoginContext } from '../../../hoc/Contexts/LoginContext';
 // Components
 import QuizzModal from './QuizzModal/QuizzModal';
-import QuizzCard from './QuizzCard/QuizzCard';
 
-const QuizzCreator = () => {
-  // State
-  const [quizzs, setQuizzs] = useState([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  // Context
-  const { user } = useContext(LoginContext);
-  const { uid } = { ...user };
-
-  // componentDidMount
-  useEffect(() => {
-    fetchUserQuizzs();
-  }, []);
-
-  // Variables
-  const userDoc = doc(db, 'users', uid);
-
-  // Methods
-  const fetchUserQuizzs = async () => {
-    console.log('FETCH USER QUIZZ');
-    const docSnap = await getDoc(userDoc);
-    if (docSnap.exists()) {
-      // console.log('Fetching ... Document ==> ', docSnap.data());
-
-      const newState = [...docSnap.data().quizzs];
-      setQuizzs(newState);
-
-      return docSnap.data();
-    } else {
-      // console.log('NO Documents ==X>');
-    }
-  };
-
-  const delHandleClick = (quizzId) => {
-    fetchUserQuizzs() // Promise
-      .then((response) => {
-        const userQuizzs = response.quizzs;
-        const userQuizzsFiltered = userQuizzs.filter((quizz) => {
-          return quizz.id !== quizzId;
-        });
-
-        setQuizzs(userQuizzsFiltered);
-
-        updateDoc(userDoc, {
-          quizzs: userQuizzsFiltered,
-        });
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const quizzsMapped = quizzs.map((quizz, index) => {
-    return (
-      <QuizzCard
-        key={index}
-        index={index}
-        quizzTitle={quizz.title !== null ? quizz.title : 'Titre du quizz'}
-        onClick={() => delHandleClick(quizz.id)}
-      />
-    );
-  });
-
-  const addQuizzClickHandler = async () => {
-    setModalIsOpen(true);
-
-    const quizzTemplate = {
-      id: Math.random(),
-      title: null,
-      questions: [],
-      tags: [],
-      thematic: [],
-      quizzPicture: null,
-    };
-
-    fetchUserQuizzs();
-
-    await setDoc(
-      userDoc,
-      {
-        quizzs: [...quizzs, quizzTemplate],
-      },
-      { merge: true }
-    );
-  };
-
-  const onSvgClickHandler = () => {
-    setModalIsOpen(false);
-  };
-
-  return modalIsOpen ? (
-    <QuizzModal modalIsOpen={modalIsOpen} onSvgClick={onSvgClickHandler} />
+const QuizzCreator = (props) => {
+  return props.modalIsOpen ? (
+    <QuizzModal modalIsOpen={props.modalIsOpen} onSvgClick={props.onSvgClickHandler} />
   ) : (
     <div className={styles.QuizzCreator}>
-      <h1>Dashboard {user.displayName}</h1>
+      <h1>Dashboard {props.user.displayName}</h1>
       <ul className={styles.wrapperQuizzs}>
-        {quizzsMapped.length > 0 ? quizzsMapped : <h2>Vous n'avez aucuns Quizz pour l'instant</h2>}
+        {props.quizzsMapped.length > 0 ? (
+          props.quizzsMapped
+        ) : (
+          <h2>Vous n'avez aucuns Quizz pour l'instant</h2>
+        )}
 
-        <div onClick={addQuizzClickHandler} className={styles.svgContainer}>
+        <div onClick={props.addQuizzClickHandler} className={styles.svgContainer}>
           <svg width='150px' height='150px' viewBox='0 0 24 24'>
             <path
               d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z'
