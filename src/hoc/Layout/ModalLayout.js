c261508834522db32837531f26e47b5de2700c03
kévin <kremadnia@gmail.com>
2022-04-11 01:23:00 +0200

    Add logo and svg into the layout && style

--- src/hoc/Layout/ModalLayout.js
+++ src/hoc/Layout/ModalLayout.js
@@ -4,13 +4,27 @@ import React from 'react';
 import logo from '../../pictures/logo.png';
 import styles from './ModalLayout.module.css';
 
-const ModalLayout = ({ children }) => {
+const ModalLayout = (props) => {
   return (
-    <div className={styles.ContainerModal}>
+    <div className={`${styles.ContainerModal} ${props.modalIsOpen ? styles.animateIn : null}`}>
       <div className={styles.ModalContent}>
+        <svg
+          className={styles.CloseModal}
+          width='2em'
+          height='2em'
+          viewBox='0 0 24 24'
+          onClick={props.onSvgClick}
+        >
+          <path
+            fill='none'
+            stroke='red'
+            strokeLinecap='round'
+            strokeWidth='2'
+            d='M20 20L4 4m16 0L4 20'
+          ></path>
+        </svg>
         <img src={logo} alt='Quizz creator écrit dans un rond de couleur bleu violet' />
-        {children}
-        <button type='button'>Suivant</button>
+        {props.children}
       </div>
     </div>
   );
