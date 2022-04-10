223b660a8f50975d6cbddc0ead8009f94a95f2e4
kévin <kremadnia@gmail.com>
2022-04-11 01:25:56 +0200

    Add comment

--- src/Components/Dashboard/QuizzCreator/QuizzCreator.js
+++ src/Components/Dashboard/QuizzCreator/QuizzCreator.js
@@ -22,6 +22,7 @@ const QuizzCreator = (props) => {
           <span>Créer un quizz</span>
         </div>
       </ul>
+      {/* Je garde la structure de la page pour ne pas la déformer quand j'ouvre la modal */}
       <QuizzModal modalIsOpen={props.modalIsOpen} onSvgClick={props.onSvgClickHandler} />
     </div>
   ) : (
