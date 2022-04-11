0adcc8ea722e702f9c15bc7582d03339710aef4d
kévin <kremadnia@gmail.com>
2022-04-11 02:45:17 +0200

    Handle active modal tab

--- src/Components/QuizzModal/AddQuestion/AddQuestion.js
+++ src/Components/QuizzModal/AddQuestion/AddQuestion.js
@@ -5,10 +5,76 @@ import logo from '../../../pictures/logo.png';
 import styles from './AddQuestion.module.css';
 
 const AddQuestion = () => {
-  // State
-  const [question, setQuestion] = React.useState([]);
+  // States
+  const [questionValue, setQuestionValue] = React.useState('');
+  const [questions, setQuestions] = React.useState([]);
 
-  return <div className={styles.AddQuestion}></div>;
+  // Methods
+  const handleAddQuestionClick = () => {
+    setQuestions([
+      ...questions,
+      {
+        id: Math.random(),
+        question: questionValue,
+        answers: [],
+      },
+    ]);
+  };
+
+  // Variables
+  const addQuestionSVG = (
+    <svg width='1.5em' height='1.5em' viewBox='0 0 512 512'>
+      <path
+        fill='none'
+        stroke='#cceac3'
+        strokeMiterlimit='10'
+        strokeWidth='32'
+        d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192s192-86 192-192Z'
+      ></path>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='32'
+        d='M256 176v160m80-80H176'
+      ></path>
+    </svg>
+  );
+
+  return (
+    <div className={styles.AddQuestion}>
+      <h3>
+        <u>Liste de vos questions</u>
+      </h3>
+      {questions.length > 0 ? (
+        <ul>
+          {questions.map((question) => {
+            return (
+              <li key={question.id}>
+                {question.question}
+                <svg width='1em' height='1em' viewBox='0 0 16 16'>
+                  <path
+                    fill='red'
+                    d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1l-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z'
+                  ></path>
+                </svg>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <h2>Aucune question</h2>
+      )}
+      <input
+        type='text'
+        placeholder='Tapez votre question...'
+        value={questionValue}
+        onChange={(e) => setQuestionValue(e.target.value)}
+      />
+      <button onClick={handleAddQuestionClick}>{addQuestionSVG} Ajouter</button>
+    </div>
+  );
 };
 
 export default AddQuestion;
--- src/Components/QuizzModal/QuizzModal.js
+++ src/Components/QuizzModal/QuizzModal.js
@@ -13,11 +13,46 @@ const QuizzModal = (props) => {
   const [modalActiveTab, setModalActiveTab] = useState(0);
   const [modalTabs, setModalTabs] = useState([<CreateQuizz />, <AddQuestion />]);
 
+  // Methods
+  const nextTabClickHandler = () => {
+    if (modalActiveTab < modalTabs.length - 1) {
+      setModalActiveTab(modalActiveTab + 1);
+    }
+  };
+
+  const previousTabClickHandler = () => {
+    if (modalActiveTab > 0) {
+      setModalActiveTab(modalActiveTab - 1);
+    }
+  };
+
+  const handleTitleModal = (modalActiveTab) => {
+    switch (modalActiveTab) {
+      case 0:
+        return 'Créer un quizz';
+      case 1:
+        return 'Liste des questions';
+      default:
+        return;
+    }
+  };
+
   return (
-    <ModalLayout modalIsOpen={props.modalIsOpen} onSvgClick={props.onSvgClick}>
-      {modalTabs[modalActiveTab]}
-      <button>Suivant</button>
-    </ModalLayout>
+    <div className={styles.QuizzModal}>
+      <ModalLayout
+        modalIsOpen={props.modalIsOpen}
+        onSvgClick={props.onSvgClick}
+        titleModal={handleTitleModal(modalActiveTab)}
+      >
+        {modalTabs[modalActiveTab]}
+        <div className={styles.buttonContainer}>
+          <button onClick={previousTabClickHandler}>
+            {modalActiveTab === 0 ? 'Annuler' : 'Précédent'}
+          </button>
+          <button onClick={nextTabClickHandler}>Suivant</button>
+        </div>
+      </ModalLayout>
+    </div>
   );
 };
 
