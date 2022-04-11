9cd3648d1880a565cbb5c70d49ea5dfa012ecf67
kévin <kremadnia@gmail.com>
2022-04-11 02:58:58 +0200

    REMOVE input and state for him

--- src/Components/QuizzModal/AddQuestion/AddQuestion.js
+++ src/Components/QuizzModal/AddQuestion/AddQuestion.js
@@ -1,24 +1,17 @@
 // Library
 import React from 'react';
 // Own files
-import logo from '../../../pictures/logo.png';
 import styles from './AddQuestion.module.css';
 
 const AddQuestion = () => {
   // States
-  const [questionValue, setQuestionValue] = React.useState('');
   const [questions, setQuestions] = React.useState([]);
 
   // Methods
-  const handleAddQuestionClick = () => {
-    setQuestions([
-      ...questions,
-      {
-        id: Math.random(),
-        question: questionValue,
-        answers: [],
-      },
-    ]);
+  const handleAddQuestionClick = () => {};
+
+  const handleDeleteClick = (id) => {
+    setQuestions(questions.filter((question) => question.id !== id));
   };
 
   // Variables
@@ -44,16 +37,18 @@ const AddQuestion = () => {
 
   return (
     <div className={styles.AddQuestion}>
-      <h3>
-        <u>Liste de vos questions</u>
-      </h3>
       {questions.length > 0 ? (
         <ul>
           {questions.map((question) => {
             return (
               <li key={question.id}>
                 {question.question}
-                <svg width='1em' height='1em' viewBox='0 0 16 16'>
+                <svg
+                  onClick={() => handleDeleteClick(question.id)}
+                  width='1em'
+                  height='1em'
+                  viewBox='0 0 16 16'
+                >
                   <path
                     fill='red'
                     d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1l-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z'
@@ -64,14 +59,8 @@ const AddQuestion = () => {
           })}
         </ul>
       ) : (
-        <h2>Aucune question</h2>
+        <h3>Aucune question</h3>
       )}
-      <input
-        type='text'
-        placeholder='Tapez votre question...'
-        value={questionValue}
-        onChange={(e) => setQuestionValue(e.target.value)}
-      />
       <button onClick={handleAddQuestionClick}>{addQuestionSVG} Ajouter</button>
     </div>
   );
