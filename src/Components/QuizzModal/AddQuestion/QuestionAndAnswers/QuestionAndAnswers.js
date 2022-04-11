cac0ffb9706641f80a95b9184b753d895e24ca2d
kévin <kremadnia@gmail.com>
2022-04-11 02:59:43 +0200

    CREATE new component --> handle questions and answers

---
+++ src/Components/QuizzModal/AddQuestion/QuestionAndAnswers/QuestionAndAnswers.js
@@ -0,0 +1,10 @@
+// Library
+import React from 'react';
+// Own Files
+import styles from './QuestionAndAnswers.module.css';
+
+const QuestionAndAnswers = () => {
+  return <div>QuestionAndAnswers</div>;
+};
+
+export default QuestionAndAnswers;
