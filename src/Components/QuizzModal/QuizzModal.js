7ad256d5f28d33003cae71f2790351a7b720493f
kévin <kremadnia@gmail.com>
2022-04-13 01:56:56 +0200

    DELETE ALL Components && RESET QuizzModal

--- src/Components/QuizzModal/QuizzModal.js
+++ src/Components/QuizzModal/QuizzModal.js
@@ -2,78 +2,27 @@
 import React, { useState } from 'react';
 // Own files
 import styles from './QuizzModal.module.css';
-// Components
-import CreateQuizz from './CreateQuizz/CreateQuizz';
-import AddQuestion from './AddQuestion/AddQuestion';
-import QuestionAndAnswers from './AddQuestion/QuestionAndAnswers/QuestionAndAnswers';
+
 //Hoc
 import { QuizzContext } from '../../hoc/Contexts/QuizzContext';
 
 const QuizzModal = (props) => {
   // State
-  const [modalActiveTab, setModalActiveTab] = useState(0);
-
-  // Methods
-  const nextTabClickHandler = () => {
-    if (modalActiveTab < modalTabs.length - 1) {
-      setModalActiveTab(modalActiveTab + 1);
-    }
-  };
-
-  const previousTabClickHandler = () => {
-    if (modalActiveTab > 0) {
-      setModalActiveTab(modalActiveTab - 1);
-    }
-  };
-
-  const handleTitleModal = (modalActiveTab) => {
-    switch (modalActiveTab) {
-      case 0:
-        return 'Créer un quizz';
-      case 1:
-        return 'Liste des questions';
-      case 2:
-        return 'Créer une question';
-      default:
-        return;
-    }
-  };
-
-  // Variables
-  const modalTabs = [
-    <CreateQuizz />,
-    <AddQuestion handleAddQuestionClick={nextTabClickHandler} />,
-    <QuestionAndAnswers />,
-  ];
-
-  const quizzTemplate = {
+  const [quizz, setQuizz] = useState({
     id: Math.random(),
     title: null,
     questions: [],
     tags: [],
     thematic: [],
     quizzPicture: null,
-  };
+  });
+  // Methods
+
+  // Variables
 
   return (
     <div className={styles.ContainerModal}>
-      <div className={styles.ModalContent}>
-        <QuizzContext.Provider value={{ quizzTemplate }}>
-          {modalTabs[modalActiveTab]}
-          <div className={styles.buttonContainer}>
-            {modalActiveTab === 2 ? (
-              <button>Confirmer</button>
-            ) : (
-              <>
-                <button onClick={previousTabClickHandler}>
-                  {modalActiveTab === 0 ? 'Annuler' : 'Précédent'}
-                </button>
-                <button onClick={nextTabClickHandler}>Suivant</button>
-              </>
-            )}
-          </div>
-        </QuizzContext.Provider>
-      </div>
+      <QuizzContext.Provider value={{ quizz }}></QuizzContext.Provider>
     </div>
   );
 };
