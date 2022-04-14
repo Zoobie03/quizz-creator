1cbd2f3d8abfc34d4b5e89d0d92ddf19e8fb0be6
Zoobie <kremadnia@gmail.com>
2022-04-14 11:42:27 +0200

    ADD display on quizz clicked

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -15,6 +15,7 @@ const Dashboard = () => {
   // State
   const [userQuizzs, setUserQuizzs] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [quizzIsClicked, setQuizzIsClicked] = useState(false);
   // Context
   const { user } = useContext(LoginContext);
   const { uid } = { ...user };
@@ -64,7 +65,10 @@ const Dashboard = () => {
         key={index}
         index={index}
         quizzTitle={quizz.title !== null ? quizz.title : 'Titre du quizz'}
-        onClick={() => delHandleClick(quizz.id)}
+        onSvgClick={() => delHandleClick(quizz.id)}
+        onQuizzClick={() => {
+          setQuizzIsClicked(!quizzIsClicked);
+        }}
       />
     );
   });
@@ -107,7 +111,7 @@ const Dashboard = () => {
         onSvgClickHandler={onSvgClickHandler}
         user={user}
       />
-      <RightColumnDashboard />
+      <RightColumnDashboard quizzIsClicked={quizzIsClicked} />
     </div>
   );
 };
