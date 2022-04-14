e1ea830ad8d3a693acca8eb4a8a6ab3e9994a48a
Zoobie <kremadnia@gmail.com>
2022-04-14 11:41:42 +0200

    ADD dislpay on quizz click

--- src/Components/Dashboard/RightColumnDashboard/RightColumnDashboard.js
+++ src/Components/Dashboard/RightColumnDashboard/RightColumnDashboard.js
@@ -3,10 +3,13 @@ import React from 'react';
 // Own Files
 import styles from './RightColumnDashboard.module.css';
 
-const RightColumnDashboard = () => {
+const RightColumnDashboard = (props) => {
   return (
-    <div className={styles.RightColumnDashboard}>
-      <h2>Statistiques</h2>
+    <div
+      className={styles.RightColumnDashboard}
+      style={{ display: props.quizzIsClicked ? 'block' : 'none' }}
+    >
+      <h2>Détails du quizz</h2>
     </div>
   );
 };
