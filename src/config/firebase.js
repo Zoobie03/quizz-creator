2db9bf5ceedb264fc74872c9028d6b3071462837
kévin <kremadnia@gmail.com>
2022-04-21 03:43:10 +0200

    CREATE .env.local file for firebase config file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyDsnrd890YIu3hQUtlD1639Ibrd1j7VGiE',
-  authDomain: 'quizz-creator.firebaseapp.com',
-  databaseURL: 'https://quizz-creator-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'quizz-creator',
-  storageBucket: 'quizz-creator.appspot.com',
-  messagingSenderId: '366969350230',
-  appId: '1:366969350230:web:e558454f343ad82c34f3f7',
-  measurementId: 'G-3QDLQDZCY5',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const auth = getAuth(firebaseApp);
