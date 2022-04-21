03d8add5faad3755053578a504bb7811f9acb8a9
kévin <kremadnia@gmail.com>
2022-04-22 00:33:38 +0200

    UPDATE .gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import firebaseInfos from '../firebase-infos';
 
 const firebaseApp = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: firebaseInfos.REACT_APP_FIREBASE_API_KEY,
+  authDomain: firebaseInfos.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: firebaseInfos.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: firebaseInfos.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: firebaseInfos.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: firebaseInfos.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: firebaseInfos.REACT_APP_FIREBASE_APP_ID,
+  measurementId: firebaseInfos.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const auth = getAuth(firebaseApp);
