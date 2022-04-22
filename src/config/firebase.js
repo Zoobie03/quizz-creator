8ebb12ee2cf5e8c3c41a1e5b1d49cd85fdc131b8
Zoobie <kremadnia@gmail.com>
2022-04-22 22:12:58 +0200

    UPDATE firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseApp = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDsnrd890YIu3hQUtlD1639Ibrd1j7VGiE',
+  authDomain: 'quizz-creator.firebaseapp.com',
+  databaseURL: 'https://quizz-creator-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'quizz-creator',
+  storageBucket: 'quizz-creator.appspot.com',
+  messagingSenderId: '366969350230',
+  appId: '1:366969350230:web:e558454f343ad82c34f3f7',
+  measurementId: 'G-3QDLQDZCY5',
 });
 
 export const auth = getAuth(firebaseApp);
