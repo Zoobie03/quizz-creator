383a233b7746ab1bbecad69fc7e882e658d7d84d
Zoobie <kremadnia@gmail.com>
2022-05-02 21:04:46 +0200

    ADD Question component

--- src/Components/Dashboard/GeneratedQuizz/GeneratedQuizz.js
+++ src/Components/Dashboard/GeneratedQuizz/GeneratedQuizz.js
@@ -2,23 +2,19 @@
 import React from 'react';
 // Own files
 import styles from './GeneratedQuizz.module.css';
+// Component
+import Question from './Question/Question';
 
 const GeneratedQuizz = (props) => {
   // quizzTitle, quizzQuestions
   return (
     <div className={styles.GeneratedQuizz}>
       <h1>{props.quizzTitle}</h1>
-      {props.quizzQuestions.map((question, index) => {
-        return (
-          <div key={index} className={styles.question}>
-            <h2>Question {index + 1}</h2>
-            <p>{question.question}</p>
-            <label htmlFor={`question${index + 1}`}>
-              <input type='text' id={`question${index + 1}`} />
-            </label>
-          </div>
-        );
-      })}
+      <ul>
+        {props.quizzQuestions.map((question, index) => {
+          return <Question index={index} question={question} />;
+        })}
+      </ul>
     </div>
   );
 };
