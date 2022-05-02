123662f217f8662e040a040517f7abffe9e92920
Zoobie <kremadnia@gmail.com>
2022-05-02 21:20:23 +0200

    REMOVE Toastcontainer

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,5 @@
 // Librairies
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 // Components
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
@@ -11,8 +10,6 @@ const Layout = (props) => {
       <Header />
 
       {props.children}
-
-      <ToastContainer position='bottom-right' newestOnTop />
 
       <Footer />
     </div>
