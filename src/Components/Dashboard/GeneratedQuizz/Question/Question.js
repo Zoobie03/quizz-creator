03c926cbacf597703b88870fa8af7dcdd8770c92
Zoobie <kremadnia@gmail.com>
2022-05-02 21:05:25 +0200

    ADD basic structure --> handle question data

--- src/Components/Dashboard/GeneratedQuizz/Question/Question.js
+++ src/Components/Dashboard/GeneratedQuizz/Question/Question.js
@@ -3,8 +3,17 @@ import React from 'react';
 // Own files
 import styles from './Question.module.css';
 
-const Question = () => {
-  return <div>Question</div>;
+const Question = (props) => {
+  // question, index
+  return (
+    <li key={props.index} className={styles.Question}>
+      <h2>Question {props.index + 1}</h2>
+      <p>{props.question.question}</p>
+      <label htmlFor={`question${props.index + 1}`}>
+        <input type='text' id={`question${props.index + 1}`} />
+      </label>
+    </li>
+  );
 };
 
 export default Question;
