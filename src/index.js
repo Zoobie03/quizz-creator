85bf97fd2dde8bbf9ddac6eb7b96e34cbfdeea9e
Zoobie <kremadnia@gmail.com>
2022-05-02 21:21:19 +0200

    ADD ToastContainer && css rules --> react-toastify

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
