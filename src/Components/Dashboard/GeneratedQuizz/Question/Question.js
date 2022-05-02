299e6290e997882005c1c7780bcd2f151c82c657
Zoobie <kremadnia@gmail.com>
2022-05-02 20:57:54 +0200

    CREATE component --> handle question in GeneratedQuizz

---
+++ src/Components/Dashboard/GeneratedQuizz/Question/Question.js
@@ -0,0 +1,10 @@
+// Library
+import React from 'react';
+// Own files
+import styles from './Question.module.css';
+
+const Question = () => {
+  return <div>Question</div>;
+};
+
+export default Question;
