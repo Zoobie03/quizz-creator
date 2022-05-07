62e52fb61a1258659545e87c78126e5b9913d62e
Zoobie <kremadnia@gmail.com>
2022-05-07 05:49:18 +0200

    UPDATE questionCard style

--- src/Components/Dashboard/GeneratedQuizz/Question/Question.js
+++ src/Components/Dashboard/GeneratedQuizz/Question/Question.js
@@ -6,11 +6,11 @@ import styles from './Question.module.css';
 const Question = (props) => {
   // question, index
   return (
-    <li key={props.index} className={styles.Question}>
-      <h2>Question {props.index + 1}</h2>
-      <p>{props.question.question}</p>
+    <li key={props.index} className={styles.QuestionCard}>
+      <span className={styles.questionTitle}>Question {props.index + 1}</span>
+      <p className={styles.question}>{props.question.question}</p>
       <label htmlFor={`question${props.index + 1}`}>
-        <input type='text' id={`question${props.index + 1}`} />
+        <input type='text' id={`question${props.index + 1}`} placeholder='Votre réponse...' />
       </label>
     </li>
   );
