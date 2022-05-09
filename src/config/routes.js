0fb015a9952a3e7a7247f15488cab6d16fbe850d
kévin <kremadnia@gmail.com>
2022-05-10 01:51:52 +0200

    FIX typo

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
 const routes = {
   HOME: '/',
-  DASHBOARD: 'dashboard',
-  CONTACT: 'contact',
-  AUTHENTIFICATION: 'authentification',
-  CREATE_ACCOUNT: 'create-account',
-  MY_PROFIL: 'profil',
-  QUIZZ: 'quizzs',
+  DASHBOARD: '/dashboard',
+  CONTACT: '/contact',
+  AUTHENTIFICATION: '/authentification',
+  CREATE_ACCOUNT: '/create-account',
+  MY_PROFIL: '/profil',
+  QUIZZ: '/quizzs',
 };
 
 export default routes;
