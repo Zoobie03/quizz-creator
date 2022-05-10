ef2a542c6df7873c81e32f7cfaa48f391a1e6401
kévin <kremadnia@gmail.com>
2022-05-11 00:16:39 +0200

    ADD basic structure && ADD basic style for Panel

--- src/Components/Dashboard/RightColumnDashboard/Panel/Panel.js
+++ src/Components/Dashboard/RightColumnDashboard/Panel/Panel.js
@@ -3,8 +3,14 @@ import React from 'react';
 // Own files
 import styles from './Panel.module.css';
 
-const Panel = () => {
-  return <div>Panel</div>;
+const Panel = (props) => {
+  // PanelTitle, quizz.questions
+  return (
+    <div className={styles.Panel}>
+      <h3>{props.PanelTitle || 'Titre générique'}</h3>
+      <p>{props?.quizz?.questions.length || 'Aucune donnée reçu en props'}</p>
+    </div>
+  );
 };
 
 export default Panel;
