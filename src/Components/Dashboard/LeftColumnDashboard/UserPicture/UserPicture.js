27d0d9566acc6b2e16f8ac6a494f63531e479027
kévin <kremadnia@gmail.com>
2022-05-11 02:32:53 +0200

    ADD default picture if the  user don't have his own

--- src/Components/Dashboard/LeftColumnDashboard/UserPicture/UserPicture.js
+++ src/Components/Dashboard/LeftColumnDashboard/UserPicture/UserPicture.js
@@ -3,6 +3,7 @@ import React from 'react';
 // Own Files
 import useAuth from '../../../../customHook/useAuth';
 import styles from './UserPicture.module.css';
+import DefaultPicture from '../../../../pictures/defaultPicture.png';
 
 const UserPicture = () => {
   const currentUser = useAuth();
@@ -10,8 +11,12 @@ const UserPicture = () => {
   return (
     <div className={styles.UserPicture}>
       {currentUser?.photoURL ? (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
         <img src={currentUser?.photoURL} alt='Image de profil' className={styles.profilPicture} />
-      ) : null}
+      ) : (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img src={DefaultPicture} alt='Image de profil' className={styles.profilPicture} />
+      )}
     </div>
   );
 };
