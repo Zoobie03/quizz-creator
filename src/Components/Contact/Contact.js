2dfc1bf5bc2d0f3c92fa4e07fb8fa3caf29a606f
kévin <kremadnia@gmail.com>
2022-05-11 05:00:27 +0200

    ADD method to handle change input state value

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,8 +5,28 @@ import styles from './Contact.module.css';
 
 const Contact = () => {
   // State
+  const [nameInputValue, setNameInputValue] = React.useState('');
+  const [emailInputValue, setEmailInputValue] = React.useState('');
+  const [messageInputValue, setMessageInputValue] = React.useState('');
 
   // Methods
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'name':
+        setNameInputValue(value);
+        break;
+      case 'email':
+        setEmailInputValue(value);
+        break;
+      case 'message':
+        setMessageInputValue(value);
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log('Formulaire soumis');
@@ -18,15 +38,27 @@ const Contact = () => {
       <form>
         <label htmlFor='name'>
           Nom
-          <input id='name' type='text' />
+          <input id='name' name='name' type='text' value={nameInputValue} onChange={handleChange} />
         </label>
         <label htmlFor='email'>
           E-mail
-          <input id='email' type='mail' />
+          <input
+            id='email'
+            name='email'
+            type='mail'
+            value={emailInputValue}
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor='message'>
           Votre message
-          <textarea id='message' rows='10'></textarea>
+          <textarea
+            id='message'
+            name='message'
+            rows='10'
+            value={messageInputValue}
+            onChange={handleChange}
+          ></textarea>
         </label>
         <button type='submit' onSubmit={(event) => handleFormSubmit(event)}>
           Envoyer
