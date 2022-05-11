1adbebc6b070ac5f466dce53aa1e5bbb3c996bb6
kévin <kremadnia@gmail.com>
2022-05-11 04:51:30 +0200

    ADD form & submit button --> with submit method

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,37 @@
-import React from "react";
+// Library
+import React from 'react';
+// Own files
+import styles from './Contact.module.css';
 
 const Contact = () => {
+  // State
+
+  // Methods
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Formulaire soumis');
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className={styles.Contact}>
+      <h1>Pour contacter le développeur</h1>
+      <form>
+        <label htmlFor='name'>
+          Nom
+          <input id='name' type='text' />
+        </label>
+        <label htmlFor='email'>
+          E-mail
+          <input id='email' type='mail' />
+        </label>
+        <label htmlFor='message'>
+          Votre message
+          <textarea id='message' rows='10'></textarea>
+        </label>
+        <button type='submit' onSubmit={(event) => handleFormSubmit(event)}>
+          Envoyer
+        </button>
+      </form>
     </div>
   );
 };
