559d54aad454c3c854ea108c4ea2e695b60c9754
Zoobie <kremadnia@gmail.com>
2022-05-13 14:24:29 +0200

    ADD props --> question picture

--- src/Components/Dashboard/GeneratedQuizz/GeneratedQuizz.js
+++ src/Components/Dashboard/GeneratedQuizz/GeneratedQuizz.js
@@ -65,6 +65,7 @@ const GeneratedQuizz = (props) => {
                       question={question}
                       questionBorderColor={isEven(index) ? '#3867edcc' : '#9603fe99'}
                       questionBorderRadius={isEven(index) ? '30px 0 30px 0' : '0 30px 0 30px'}
+                      picture={props.picture}
                     />
                   );
                 })
