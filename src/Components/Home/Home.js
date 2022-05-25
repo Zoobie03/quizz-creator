40d294d01849931e2d74720fd4f1858ac6477138
Zoobie <kremadnia@gmail.com>
2022-05-25 21:12:30 +0200

    ADD content

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,6 +14,21 @@ const Home = () => {
       {user ? (
         <div className={styles.Home}>
           <h1>Quizz Creator</h1>
+          <div className={styles.Home_Content}>
+            <div className={styles.leftColumn}>
+              <h2>Bienvenue à toi !</h2>
+              <p>
+                Avant de commencer, pense a vérifier ton e-mail et a entrer un pseudonyme dans
+                l'onglet <i>Mon profil</i>. Tu peux aussi t'attribué une image de profil.
+              </p>
+              <hr />
+              <h2>Par où commencer ?</h2>
+              <p>
+                Rendez-vous dans l'onglet <i>Dashboard</i> pour créer votre premier quizz.
+              </p>
+            </div>
+            <div className={styles.rightColumn}></div>
+          </div>
         </div>
       ) : (
         <div className={styles.Home}>
